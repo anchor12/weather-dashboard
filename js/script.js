27bf5f5d41f04aabe82758d5b4a165189d4bff9f
Natalie Carr <natticarr@gmail.com>
2019-12-09 23:38:09 +1100

    working search bar and temp, humidity and wind speed data appears

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var apikey = "c87501e8b4c8cfd8b54fbd772545c8d3";
     var searchedCity;
-    
 
-    $("#search-button").on("click", function() {
+    function getDateFunction() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '/' + dd + '/' + yyyy;
+        return today;
+    }
+
+
+    $("#search-button").on("click", function () {
         var savedButton = $("<button>");
         savedButton.text($("#search-bar").val());
         savedButton.addClass("btn btn-outline-secondary");
-        savedButton.css("margin-bottom","5px");
-        $(".card-body").append(savedButton);
-        $(".card-body").append($("<br>"));
+        savedButton.css("margin-bottom", "5px");
+        $(".search-term-box").append(savedButton);
+        $(".search-term-box").append($("<br>"));
         searchedCity = $("#search-bar").val();
         console.log(searchedCity);
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apikey;
@@ -19,32 +29,27 @@ $(document).ready(function() {
         $.ajax({
             url: queryURL,
             method: "GET"
-          })
+        })
             // We store all of the retrieved data inside of an object called "response"
-            .then(function(response) {
-      
-              // Log the queryURL
-              console.log(queryURL);
-      
-              // Log the resulting object
-              console.log(response);
-      
-              // Transfer content to HTML
-              $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-              $(".wind").text("Wind Speed: " + response.wind.speed);
-              $(".humidity").text("Humidity: " + response.main.humidity);
-              $(".temp").text("Temperature (F) " + response.main.temp);
-      
-              // Converts the temp to Kelvin with the below formula
-              var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-              $(".tempF").text("Temperature (Kelvin) " + tempF);
-      
-              // Log the data in the console as well
-              console.log("Wind Speed: " + response.wind.speed);
-              console.log("Humidity: " + response.main.humidity);
-              console.log("Temperature (F): " + response.main.temp);
+            .then(function (response) {
+
+                // Log the queryURL
+                console.log(queryURL);
+
+                // Log the resulting object
+                console.log(response);
+
+                // Transfer content to HTML
+
+                $(".city-name").html("<h2>" + response.name + " (" + getDateFunction() + ")</h2><hr>");
+
+                $(".city-temp").text("Temperature (F) " + response.main.temp);
+                $(".city-humidity").text("Humidity: " + response.main.humidity);
+                $(".city-wind").text("Wind Speed: " + response.wind.speed);
+
+
             });
-        
+
     });
 
 
